4ccef094eed937b186a17bf114abd4f16661b9a0
Pame026 <53342991+Pame026@users.noreply.github.com>
2020-01-28 22:24:05 -0300

    add items endpoint

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,45 +2,43 @@ var express = require("express");
 var router = express.Router();
 const axios = require("axios");
 
-const search = "https://api.mercadolibre.com/sites/MLA/search?q=";
-const q = "mesas";
-var query;
-
-router.get("/", function(req, res, next) {
-  res.send(consulta);
-  console.log(consulta);
-});
-
-router.get("/items?q=", function(req, res, next) {
-  axios
-    .get(search + q)
-    .then(e => {
-      query = {
-        author: {
-          name: "Pame",
-          lastname: "Torres Lujan"
-        },
-        categories: [],
-        items: e.data.results.map(element => {
-          return {
-            id: element.id,
-            title: element.title,
-            price: {
-              currency: element.currency_id,
-              amount: Math.trunc(element.installments.amount),
-              decimals:
-                element.installments.amount -
-                Math.trunc(element.installments.amount)
-            },
-            picture: element.thumbnail,
-            condition: element.condition,
-            free_shipping: element.shipping.free_shipping
-          };
-        })
-      };
+router.get("/items", async (req, res) => {
+  try{
+    const response = await axios.get(`https://api.mercadolibre.com/sites/MLA/search?q=${req.query.q}`)
+    res.json({
+      author: {
+        name: "Pame",
+        lastname: "Torres Lujan"
+      },
+      categories: [],
+      items: response.data.results.map(element => {
+        return {
+          id: element.id,
+          title: element.title,
+          price: {
+            currency: element.currency_id,
+            amount: Math.trunc(element.installments.amount),
+            decimals:
+                    element.installments.amount -
+                    Math.trunc(element.installments.amount)
+          },
+          picture: element.thumbnail,
+          condition: element.condition,
+          free_shipping: element.shipping.free_shipping
+        }
+      })
     })
-    .catch(console.log("error"));
-
-  res.send(query);
+  }catch(error){
+    if (error.response) {
+      res
+        .status(error.response.status)
+        .json({ error: error.response.data.message });
+    }
+  }
 });
+
+router.get("/test", function(req, res, next) {
+  res.send('respond with a resource');
+});
+
 module.exports = router;
