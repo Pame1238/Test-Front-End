ac3077c203b377c9a14f001ae253dc22341f79b2
Pame026 <53342991+Pame026@users.noreply.github.com>
2020-01-29 20:19:42 -0300

    add endpoints

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,54 +2,79 @@ var express = require("express");
 var router = express.Router();
 const axios = require("axios");
 
-// const search = "https://api.mercadolibre.com/sites/MLA/search?q=";
-// const categoryUrl = "https://api.mercadolibre.com/categories/";
-// var category_id = "";
-// var con = [];
-// var cat = [];
-// var c = [];
-// const q = "Apple ipod";
-// var consulta = [];
+router.get("/items", async (req, res) => {
 
-//categories
+  try{
+    const response = await axios.get(`https://api.mercadolibre.com/sites/MLA/search?q=${req.query.q}`)
 
-//consulta
-// axios
-//   .get(search + q)
-//   .then(e => {
-//     consulta = e.data.results.map(element => {
-//       return {
-//         author: {},
-//         categories: cat,
-//         items: [
-//           {
-//             id: element.id,
-//             title: element.title,
-//             price: {},
-//             picture: "",
-//             condition: "",
-//             free_shipping: ""
-//           }
-//         ]
-//       };
-//     });
-
-//     console.log(consulta);
-//   })
-//   .catch(console.log("error"));
-
-router.get("/", async (req, res, next) => {
-  try {
-    const response = await axios.get(
-      `https://api.mercadolibre.com/sites/MLA/search?q=${req.query.q}`
-    );
-
-    const categories = response.data.filters.find(
-      filter => filter.id === "category"
-    );
+     const categories_id = response.data.results.map(e =>{
+      return e.category_id
+    });
+    
+    const filtro_categories = categories_id.filter((e, index, arr) =>{
+      return arr.indexOf(e) === index;
+    });
+    
+    res.json({
+      author: {
+        name: "Pame",
+        lastname: "Torres Lujan"
+      },
+      categories: filtro_categories,
+      items: response.data.results.map(element => {
+        return {
+          id: element.id,
+          title: element.title,
+          price: {
+            currency: element.currency_id,
+            amount: element.price
+          },
+          picture: element.thumbnail,
+          condition: element.condition,
+          free_shipping: element.shipping.free_shipping
+        }
+      })
+    })
+  }catch(error){
+    if (error.response) {
+      res
+        .status(error.response.status)
+        .json({ error: error.response.data.message });
+    }
   }
-
-  res.send(cat);
 });
+
+router.get("/items/:id", async (req, res) =>{
+  try{
+    const response = await axios.get(`https://api.mercadolibre.com/items/${req.params.id}`)
+    const description = await axios.get(`https://api.mercadolibre.com/items/${req.params.id}/descriptions`)
+    res.json({
+        author: {
+        name: "Pame",
+        lastname: "Torres Lujan"
+      },
+      item: {
+        id: response.data.id,
+        title: response.data.title,
+        price: {
+          currency: response.data.currency_id,
+          amount: response.data.price
+        },
+        picture: response.data.thumbnail,
+        condition: response.data.condition,
+        free_shipping: response.data.shipping.free_shipping,
+        sold_quantity: response.data.sold_quantity,
+        description: description.data[0].plain_text
+
+      }
+    })
+  }catch(error){
+    if (error.response) {
+      res
+        .status(error.response.status)
+        .json({ error: error.response.data.message });
+    }
+  }
+}); 
 
 module.exports = router;
