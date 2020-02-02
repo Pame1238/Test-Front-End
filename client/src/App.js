b790497f8de2747349d3d99045e3a1c0e47f3534
Pame026 <53342991+Pame026@users.noreply.github.com>
2020-02-02 02:45:42 -0300

    display response data

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,37 @@
 import React, { Component } from "react";
 import "./App.css";
-import Layout from "./components/Layout";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      locading: true,
+      consulta: "",
+      responseJSON: {}
+    }
+  }
+  
+componentDidMount(){
+  
+  fetch('/api/items?q=mesas')
+            .then(res => {
+                console.log(res);
+                return res.json()
+             })
+            .then(response => { 
+                console.log('json',response); 
+                this.setState({ responseJSON: response })
+                this.setState({ locading: false })
+             });
+
+}
+
   render() {
+    const r = this.state.responseJSON
     return (
       <div className="App">
-        <Layout></Layout>
+        {this.state.locading ? <div>loading...</div> : <div>response... </div>}
+        {Object.keys(r).map((e, i) => {return <div key={i}>{e}</div>})}
       </div>
     );
   }
